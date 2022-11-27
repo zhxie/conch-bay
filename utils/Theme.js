4ed1d88e3a52bcc5a2b21f399e38dab3e9a7e537
Xie Zhihao <xzh1206@gmail.com>
2022-11-27 22:35:53 +0800

    Delete duplicate files

--- utils/Theme.js
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from "native-base";
-import Color from "./color";
-
-const config = {
-  useSystemColorMode: true,
-};
-const theme = extendTheme({ config: config, colors: Color });
-
-export default theme;
