ee70f0654f6d711f6c790cfb422d13ae2e85d9cc
Xie Zhihao <xzh1206@gmail.com>
2022-12-20 18:55:32 +0800

    Add tools and CI scripts (#2)

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+  root: true,
+};
