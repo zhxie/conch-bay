78831251cb688c9bd975507f60ccbadecf6d1e78
Xie Zhihao <xzh1206@gmail.com>
2023-02-11 17:23:20 +0800

    Bump build version to 36

--- app.config.js
+++ app.config.js
@@ -25,7 +25,7 @@ const config = {
   jsEngine: "hermes",
   ios: {
     bundleIdentifier: "name.sketch.ConchBay",
-    buildNumber: "35",
+    buildNumber: "36",
     config: {
       usesNonExemptEncryption: false,
     },
@@ -44,7 +44,7 @@ const config = {
   },
   android: {
     package: "name.sketch.conch_bay",
-    versionCode: 35,
+    versionCode: 36,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#6B84F5",
