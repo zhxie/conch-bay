9fa1d1fd46ca6a59a5b8c73b019f6cf84080d040
Xie Zhihao <xzh1206@gmail.com>
2023-04-24 22:32:26 +0800

    Bump build version to 51

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ const config = {
   },
   ios: {
     bundleIdentifier: "name.sketch.ConchBay",
-    buildNumber: "50",
+    buildNumber: "51",
     config: {
       usesNonExemptEncryption: false,
     },
@@ -43,7 +43,7 @@ const config = {
   },
   android: {
     package: "name.sketch.conch_bay",
-    versionCode: 50,
+    versionCode: 51,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#6B84F5",
