33a96bb422573da4e66315f57f1ba6490aac2888
Xie Zhihao <xzh1206@gmail.com>
2023-07-08 00:05:05 +0800

    Optimize special weapon overlapping UI

--- components/WaveBox.tsx
+++ components/WaveBox.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { StyleProp, ViewStyle } from "react-native";
 import Image, { ImageSource } from "./Image";
 import Marquee from "./Marquee";
@@ -22,32 +21,6 @@ interface WaveBoxProps {
 
 const WaveBox = (props: WaveBoxProps) => {
   const theme = useTheme();
-
-  const specialWeapons = useMemo(() => {
-    const result: { image: ImageSource; count: number }[] = [];
-    const map = new Map<string, number>();
-    for (const specialWeapon of props.specialWeapons) {
-      let i = map.get(specialWeapon.uri!);
-      if (i === undefined) {
-        result.push({ image: specialWeapon, count: 0 });
-        map.set(specialWeapon.uri!, result.length - 1);
-        i = map.get(specialWeapon.uri!)!;
-      }
-      result[i].count += 1;
-    }
-    return result;
-  }, [props.specialWeapons]);
-  const overlapping = useMemo(() => {
-    const basic =
-      20 * specialWeapons.length + ViewStyles.mr1.marginRight * (specialWeapons.length - 1);
-    const extraCount = specialWeapons.reduce((prev, current) => prev + current.count - 1, 0);
-    const each = (125 - basic) / extraCount;
-    if (each > 20 + ViewStyles.mr1.marginRight) {
-      return 0;
-    } else {
-      return Math.ceil(20 + ViewStyles.mr1.marginRight - each);
-    }
-  }, [specialWeapons]);
 
   return (
     <VStack center style={[{ width: 125 }, props.style]}>
@@ -91,47 +64,26 @@ const WaveBox = (props: WaveBoxProps) => {
       </VStack>
       {props.specialWeaponPadding && (
         <HStack style={{ height: 20 }}>
-          {specialWeapons.map((specialWeapon, i, specialWeapons) => (
-            <HStack key={i} style={i !== specialWeapons.length - 1 && ViewStyles.mr1}>
-              {new Array(specialWeapon.count).fill(0).map((_, j) => (
-                <Center
-                  key={j}
-                  style={[
-                    j !== 0 && {
-                      marginLeft: 3 - overlapping,
-                      // HACK: the margin is consisted of a 1 border and a 3 margin.
-                      borderLeftWidth: 1,
-                      borderLeftColor: theme.backgroundColor,
-                    },
-                  ]}
-                >
-                  <Circle size={20} color={theme.territoryColor} style={ViewStyles.r1} />
-                  <Image
-                    source={specialWeapon.image}
-                    style={[
-                      ViewStyles.transparent,
-                      // HACK: have to designate a left value to avoid offset by border.
-                      { width: 15, height: 15, position: "absolute", left: 2.5 },
-                    ]}
-                  />
-                </Center>
-              ))}
-              {/* An extra background to avoid display error when overlapping with radius and border. */}
-              {specialWeapon.count > 1 && overlapping > 3 && (
-                <Center
-                  style={[
-                    theme.territoryStyle,
-                    ViewStyles.r1,
-                    {
-                      position: "absolute",
-                      width: "100%",
-                      height: "100%",
-                      zIndex: -1,
-                    },
-                  ]}
-                />
-              )}
-            </HStack>
+          {props.specialWeapons.map((specialWeapon, i, specialWeapons) => (
+            <Center
+              key={i}
+              style={[
+                i !== specialWeapons.length - 1 && ViewStyles.mr1,
+                i > 0 && specialWeapon.uri === specialWeapons[i - 1].uri && { marginLeft: -16 },
+                { zIndex: -i },
+              ]}
+            >
+              <Circle
+                size={20}
+                color={theme.backgroundColor}
+                style={[ViewStyles.r1, { position: "absolute", left: 1 }]}
+              />
+              <Circle size={20} color={theme.territoryColor} style={ViewStyles.r1} />
+              <Image
+                source={specialWeapon}
+                style={[ViewStyles.transparent, { width: 15, height: 15, position: "absolute" }]}
+              />
+            </Center>
           ))}
         </HStack>
       )}
