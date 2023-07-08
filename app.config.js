31ab03862d37247369a23475f69db56a8a41f3be
Xie Zhihao <xzh1206@gmail.com>
2023-07-08 22:02:38 +0800

    Bump build version to 90

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ const config = {
   },
   ios: {
     bundleIdentifier: "name.sketch.ConchBay",
-    buildNumber: "89",
+    buildNumber: "90",
     config: {
       usesNonExemptEncryption: false,
     },
@@ -44,7 +44,7 @@ const config = {
   },
   android: {
     package: "name.sketch.conch_bay",
-    versionCode: 89,
+    versionCode: 90,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#6B84F5",
