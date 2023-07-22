711b1a92fb9c8ebe6a4908589d7db11be98ca057
Xie Zhihao <xzh1206@gmail.com>
2023-07-23 00:55:55 +0800

    Bump build version to 98

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ const config = {
   },
   ios: {
     bundleIdentifier: "name.sketch.ConchBay",
-    buildNumber: "97",
+    buildNumber: "98",
     config: {
       usesNonExemptEncryption: false,
     },
@@ -44,7 +44,7 @@ const config = {
   },
   android: {
     package: "name.sketch.conch_bay",
-    versionCode: 97,
+    versionCode: 98,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#6B84F5",
