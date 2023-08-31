4825daf08776535f81c26a5c08ac2b931f62b302
Xie Zhihao <xzh1206@gmail.com>
2023-08-31 20:24:12 +0800

    Bump build version to 122

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ const config = {
   },
   ios: {
     bundleIdentifier: "name.sketch.ConchBay",
-    buildNumber: "121",
+    buildNumber: "122",
     config: {
       usesNonExemptEncryption: false,
     },
@@ -44,7 +44,7 @@ const config = {
   },
   android: {
     package: "name.sketch.conch_bay",
-    versionCode: 121,
+    versionCode: 122,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#6B84F5",
