12b035da13e13b6aad6f31aef38d0a949fb0dfc5
Xie Zhihao <xzh1206@gmail.com>
2024-04-25 17:14:26 +0800

    Bump build version to 147

--- app.config.js
+++ app.config.js
@@ -27,7 +27,7 @@ const config = {
   },
   ios: {
     bundleIdentifier: IS_DEV ? "name.sketch.ConchBay.dev" : "name.sketch.ConchBay",
-    buildNumber: "146",
+    buildNumber: "147",
     config: {
       usesNonExemptEncryption: false,
     },
@@ -67,7 +67,7 @@ const config = {
   },
   android: {
     package: IS_DEV ? "name.sketch.conch_bay.dev" : "name.sketch.conch_bay",
-    versionCode: 146,
+    versionCode: 147,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#6B84F5",
