90ad35725cea4afc7cc7c36a7a7f240be0b5e07e
Xie Zhihao <xzh1206@gmail.com>
2024-04-30 22:35:22 +0800

    Add link to raw OSS licenses file since GitHub may not render the full page

--- tools/gen-oss-licenses-md.mjs
+++ tools/gen-oss-licenses-md.mjs
@@ -4,6 +4,9 @@ import { init } from "license-checker";
 const licenseFile = createWriteStream("oss-licenses.md", "utf-8");
 
 init({ start: ".", customFormat: { licenseText: "" } }, (_, ret) => {
+  licenseFile.write(
+    "*See the [raw](https://github.com/zhxie/conch-bay/wiki/OSS-Licenses.md) file if the page does not render.*\n\n"
+  );
   for (const name in ret) {
     const repository = ret[name]["repository"] ?? "";
     const licenseText = ret[name]["licenseText"] ?? "";
